2f8f1d6e43b931d84e73df17677a303008ae3d4b
juanpenagos18 <jjpm2012@hotmail.com>
2026-05-13 20:44:39 -0500

    Corrección Modulo 1 y 2

--- Frontend/src/components/layout/Layout.jsx
+++ Frontend/src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from "../Header";
+import Footer from "../Footer";
 
 /**
  * Layout global — envuelve todas las páginas con Header + Footer.
